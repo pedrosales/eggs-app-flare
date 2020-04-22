16114710a78c4cc15aecbbd5e378abce0f8c8854
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-22 02:12:04 -0300

    implementando bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:eggs/blocs/app.bloc.dart';
 import 'package:eggs/pages/splash.page.dart';
 import 'package:eggs/themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppBloc>.value(
+          value: AppBloc(),
+        ),
+      ],
+      child: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
